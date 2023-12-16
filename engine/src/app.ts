d64c84b4def270304460e325f2ada7c760312646
Plinio Mabesi <pliniomabesi@gmail.com>
2023-12-16 14:16:33 -0300

    Commons library initial setup

--- engine/src/app.ts
+++ engine/src/app.ts
@@ -1,4 +1,5 @@
 import CFG from "./config";
+import { Token } from "commons/models/token";
 
 console.log("Hello world!!!");
-console.log(CFG.EXCHANGE);
+console.log(CFG.EXCHANGE2);
--- engine/src/config.ts
+++ engine/src/config.ts
@@ -1,9 +1,31 @@
+import { ChainId } from "commons/models/chainId";
+import { Exchange } from "commons/models/exchange";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const NETWORK : string = `${process.env.NETWORK}`;
+
+function getNetwork(network: string) : ChainId {
+    switch (network) {
+        case "goerli": return ChainId.GOERLI;
+        default: return ChainId.MAINNET;
+    }
+}
+
+const NETWORK2: ChainId = getNetwork(NETWORK);
+
 const EXCHANGE : string = `${process.env.EXCHANGE}`;
+
+function getExchange(exchange: string): Exchange {
+    switch (exchange) {
+        case "pancakeswap": return Exchange.PancakeSwap;
+        default: return Exchange.Uniswap;
+    }
+}
+
+const EXCHANGE2 : Exchange = getExchange(EXCHANGE);
+
 const DATABASE_URL : string = `${process.env.DATABASE_URL}`;
 const UNISWAP_GRAPH_URL : string = `${process.env.UNISWAP_GRAPH_URL}`;
 const MONITOR_INTERVAL : number = parseInt(`${process.env.MONITOR_INTERVAL}`);
@@ -11,7 +33,9 @@ const POOL_COUNT : number = parseInt(`${process.env.POOL_COUNT}`);
 
 export default {
     NETWORK,
+    NETWORK2,
     EXCHANGE,
+    EXCHANGE2,
     DATABASE_URL,
     UNISWAP_GRAPH_URL,
     MONITOR_INTERVAL,
