d35c76fbc800778b04e8ba889d56f58374f4b9aa
Plinio Mabesi <pliniomabesi@gmail.com>
2023-12-17 11:32:23 -0300

    Funcion updatePrices

--- engine/src/app.ts
+++ engine/src/app.ts
@@ -1,14 +1,35 @@
 import CFG from "./config";
-import { Token } from "commons/models/token";
 import { getTopPools } from "./services/uniswapService";
+import poolsRepository from "./repositories/poolsRepository";
 
-console.log("Inicializando CERBERUS Monitor");
-console.log("Obtendo Uniswap Pools");
+async function executionCycle() {
+    
+    console.log(new Date() + ' - Updating monitor...');
 
-getTopPools()
-    .then(data => {
-        console.log(data);
-    })
-    .catch(err => {
-        console.error(err);
-    })
+    const pages = Math.ceil(CFG.POOL_COUNT / 1000);
+
+    for (let i=0; i < pages; i++ ) {
+        
+        const pools = await getTopPools(1000, i * 1000);
+        console.log(`Loaded ${pools.length} pools`);
+
+        for (let j=0; j < pools.length; j++) {
+            
+            const pool = pools[j];
+            
+            const result = await poolsRepository.updatePrices(pool);
+            if (!result) continue;
+            
+            console.log(`Price for ${result.symbol} (${result.fee / 10000}%) is ${Number(result.price0).toFixed(6)}`);
+        }
+
+    }
+
+    console.log(`----- Pausing update for ${CFG.MONITOR_INTERVAL / 60000} minutes -----`);
+}
+
+setInterval(executionCycle, CFG.MONITOR_INTERVAL);
+
+console.log(`Cerberus Monitor started...`);
+
+executionCycle();
--- engine/src/repositories/poolsRepository.ts
+++ engine/src/repositories/poolsRepository.ts
@@ -1,7 +1,9 @@
-import { ChainId } from "commons/models/chainId";
+import CFG from "../config";
 import connect from "./db";
 import Pool from "commons/models/pool";
+import { ChainId } from "commons/models/chainId";
 import { Exchange } from "commons/models/exchange";
+import { PoolData } from "../services/uniswapTypes";
 
 
 async function countPools(exchange: Exchange, network: ChainId): Promise<number> {
@@ -35,8 +37,45 @@ async function addPool(pool: Pool): Promise<Pool> {
     return newPool;
 }
 
+async function updatePrices(poolData: PoolData) : Promise<Pool | null> {
+
+    const newPrice0 = Number(poolData.token0Price);
+    const newPrice1 = Number(poolData.token1Price);
+
+    const db = await connect();
+    let pool = await getPool(poolData.id);
+    if (!pool) {
+        pool = new Pool({
+            exchange: CFG.EXCHANGE2,
+            network: CFG.NETWORK2,
+            id: poolData.id,
+            fee: Number(poolData.feeTier),
+            token0: poolData.token0.id,
+            token1: poolData.token1.id,
+            symbol: poolData.token0.symbol + poolData.token1.symbol,
+            symbol0: poolData.token0.symbol,
+            symbol1: poolData.token1.symbol,
+        } as Pool);
+        pool = await addPool(pool);
+    }
+
+    //TODO: Processamentos adicionais
+
+    await db.pools.update({
+        where: { id: poolData.id },
+        data: {
+            price0: poolData.token0Price,
+            price1: poolData.token1Price,
+            lastUpdate: new Date()
+        }
+    });
+
+    return getPool(poolData.id);
+}
+
 export default {
     getPool,
     addPool,
-    countPools
+    countPools,
+    updatePrices
 }
