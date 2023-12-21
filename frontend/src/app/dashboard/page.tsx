e6d0fd50b369c5fa9e07655649ffd493e2045445
Plinio Mabesi <pliniomabesi@gmail.com>
2023-12-21 11:21:45 -0300

    Set TopPools component

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -9,7 +9,7 @@ import HeaderStats from "@/components/Headers/HeaderStats.js";
 import FooterAdmin from "@/components/Footers/FooterAdmin.js";
 import CardLineChart from "@/components/Cards/CardLineChart.js";
 import CardBarChart from "@/components/Cards/CardBarChart.js";
-import CardPageVisits from "@/components/Cards/CardPageVisits.js";
+import CardTopPools from "@/components/Cards/CardTopPools";
 import CardSocialTraffic from "@/components/Cards/CardSocialTraffic.js";
 
 export default function Dashboard() {
@@ -30,7 +30,7 @@ export default function Dashboard() {
                 </div>
                 <div className="flex flex-wrap mt-4">
                     <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-                    <CardPageVisits />
+                    <CardTopPools />
                     </div>
                     <div className="w-full xl:w-4/12 px-4">
                     <CardSocialTraffic />
