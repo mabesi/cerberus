190d29b7b7f56586b5b149a2cbf0cf8d509610c4
Plinio Mabesi <pliniomabesi@gmail.com>
2023-12-22 20:05:34 -0300

    Create config in backend from configBase

---
+++ backend/src/config.ts
@@ -0,0 +1,10 @@
+import ConfigBase from "commons/configBase";
+
+export default class Config extends ConfigBase {
+    
+    static PORT: number = parseInt(`${process.env.PORT || 3001}`);
+
+    static CHAIN_ID: number = parseInt(`${process.env.CHAIN_ID || 1}`);
+
+    static AUTH_MSG: string = `${process.env.AUTH_MSG || "Authenticating to Cerberus. Timestamp: <timestamp>"}`;    
+}
