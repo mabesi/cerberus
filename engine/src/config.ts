5f4f3623da9bafde104343934ff79136b3b3d4fb
Plinio Mabesi <pliniomabesi@gmail.com>
2023-12-22 19:48:09 -0300

    Update Engine config

--- engine/src/config.ts
+++ engine/src/config.ts
@@ -1,55 +1,36 @@
+import ConfigBase from "commons/configBase";
 import { ChainId } from "commons/models/chainId";
 import { Exchange } from "commons/models/exchange";
-import dotenv from "dotenv";
 
-dotenv.config();
+export default class Config extends ConfigBase {
 
-// Network
-const NETWORK : string = `${process.env.NETWORK}`;
-
-function getNetwork(network: string) : ChainId {
-    switch (network) {
-        case "goerli": return ChainId.GOERLI;
-        default: return ChainId.MAINNET;
+    // Network
+    static NETWORK : string = `${process.env.NETWORK}`;
+    static getNetwork(network: string) : ChainId {
+        switch (network) {
+            case "goerli": return ChainId.GOERLI;
+            default: return ChainId.MAINNET;
+        }
     }
-}
+    static NETWORK2: ChainId = Config.getNetwork(Config.NETWORK);
 
-const NETWORK2: ChainId = getNetwork(NETWORK);
-
-// Exchange
-const EXCHANGE : string = `${process.env.EXCHANGE}`;
-
-function getExchange(exchange: string): Exchange {
-    switch (exchange) {
-        case "pancakeswap": return Exchange.PancakeSwap;
-        default: return Exchange.Uniswap;
+    // Exchange
+    static EXCHANGE : string = `${process.env.EXCHANGE}`;
+    static getExchange(exchange: string): Exchange {
+        switch (exchange) {
+            case "pancakeswap": return Exchange.PancakeSwap;
+            default: return Exchange.Uniswap;
+        }
     }
-}
+    static EXCHANGE2 : Exchange = Config.getExchange(Config.EXCHANGE);
 
-const EXCHANGE2 : Exchange = getExchange(EXCHANGE);
+    // Uniswap GraphQL API
+    static UNISWAP_GRAPH_URL : string = `${process.env.UNISWAP_GRAPH_URL}`;
+    // Update Interval
+    static MONITOR_INTERVAL : number = parseInt(`${process.env.MONITOR_INTERVAL}`);
+    // Total Pool Count
+    static POOL_COUNT : number = parseInt(`${process.env.POOL_COUNT}`);
+    // WebSocket Server
+    static WS_PORT : number = parseInt(`${process.env.WS_PORT}`);
 
-// Mongo Database
-const DATABASE_URL : string = `${process.env.DATABASE_URL}`;
-
-// Uniswap GraphQL API
-const UNISWAP_GRAPH_URL : string = `${process.env.UNISWAP_GRAPH_URL}`;
-
-// Update Interval
-const MONITOR_INTERVAL : number = parseInt(`${process.env.MONITOR_INTERVAL}`);
-// Total Pool Count
-const POOL_COUNT : number = parseInt(`${process.env.POOL_COUNT}`);
-
-// WebSocket Server
-const WS_PORT : number = parseInt(`${process.env.WS_PORT}`);
-
-export default {
-    NETWORK,
-    NETWORK2,
-    EXCHANGE,
-    EXCHANGE2,
-    DATABASE_URL,
-    UNISWAP_GRAPH_URL,
-    MONITOR_INTERVAL,
-    POOL_COUNT,
-    WS_PORT
 }
