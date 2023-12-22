baa2267a0dd2c549b1900f8588dc6e8d40e1e1a2
Plinio Mabesi <pliniomabesi@gmail.com>
2023-12-22 17:39:12 -0300

    Back token address to Goerli

--- frontend/src/app/pay/[wallet]/page.tsx
+++ frontend/src/app/pay/[wallet]/page.tsx
@@ -23,8 +23,8 @@ export default function Pay() {
   const [plan, setPlan] = useState<Plan>({
     name: "Gold",
     id: "Gold",
-    tokenSymbol: "WMATIC",
-    tokenAddress: "0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889",
+    tokenSymbol: "WETH",
+    tokenAddress: "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6",
     price: ethers.parseEther("0.001").toString(),
     maxAutomations: 10
   });
