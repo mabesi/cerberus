0383502d0f379a00b8ea27c4ff9179d5491bdff8
Plinio Mabesi <pliniomabesi@gmail.com>
2023-12-23 09:02:50 -0300

    CORS + Pipe validation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,21 @@
 import CFG from "./config";
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
+
+function serverOk() {
+  console.log(`Running backend at ${CFG.PORT}`);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(CFG.PORT);
+
+  app.enableCors({
+    origin: CFG.CORS_ORIGIN
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(CFG.PORT, () => serverOk());
 }
 bootstrap();
