a80128b0e79cfa66258b261adbd90ae60ab0d26d
Plinio Mabesi <pliniomabesi@gmail.com>
2024-01-02 21:55:58 -0300

    Create AutomationService on frontend

---
+++ frontend/src/services/AutomationService.ts
@@ -0,0 +1,45 @@
+import axios from "./BaseService";
+import ConfigService from "./ConfigService";
+import Automation from "commons/models/automation";
+
+const AUTOMATIONS_URL = `${ConfigService.BACKEND_URL}/automations`;
+
+export async function getAutomation(id: string): Promise<Automation> {
+    const response = await axios.get(`${AUTOMATIONS_URL}/${id}`);
+    return response.data as Automation;
+}
+
+export async function getAutomations(page: number = 1, pageSize: number = 20): Promise<Automation[]> {
+    const response = await axios.get(`${AUTOMATIONS_URL}/?page=${page}&pageSize=${pageSize}`);
+    return response.data as Automation[];
+}
+
+export async function getActiveAutomations(): Promise<Automation[]> {
+    const response = await axios.get(`${AUTOMATIONS_URL}/active`);
+    return response.data as Automation[];
+}
+
+export async function addAutomation(automation: Automation): Promise<Automation> {
+    const response = await axios.post(`${AUTOMATIONS_URL}/`, automation);
+    return response.data as Automation;
+}
+
+export async function startAutomation(id: string): Promise<Automation> {
+    const response = await axios.post(`${AUTOMATIONS_URL}/${id}/start`);
+    return response.data as Automation;
+}
+
+export async function stopAutomation(id: string): Promise<Automation> {
+    const response = await axios.post(`${AUTOMATIONS_URL}/${id}/stop`);
+    return response.data as Automation;
+}
+
+export async function updateAutomation(id: string, automation: Automation): Promise<Automation> {
+    const response = await axios.patch(`${AUTOMATIONS_URL}/${id}`, automation);
+    return response.data as Automation;
+}
+
+export async function deleteAutomation(id: string): Promise<Automation> {
+    const response = await axios.delete(`${AUTOMATIONS_URL}/${id}`);
+    return response.data as Automation;
+}
