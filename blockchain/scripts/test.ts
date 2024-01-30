4e23ca537a7d40c722d52e2c5ca7ad9c99c6d2e6
Plinio Mabesi <pliniomabesi@gmail.com>
2024-01-30 08:53:29 -0300

    Adjust blockchain tests

--- blockchain/scripts/test.ts
+++ blockchain/scripts/test.ts
@@ -1,17 +1,17 @@
 import { ethers } from "hardhat";
+import { CerberusPay } from "../typechain-types";
 
 const ABI_WETH = require("./abi.weth.json");
-
 const WETH_MAINNET = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
 const WALLET = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
 const PRIVATE_KEY = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
 
+const provider = new ethers.JsonRpcProvider("http://localhost:8545");
+const signer = new ethers.Wallet(PRIVATE_KEY, provider);
+const wethContract = new ethers.Contract(WETH_MAINNET, ABI_WETH, signer);
+
 async function depositEth(){
     
-    const provider = new ethers.JsonRpcProvider("http://localhost:8545");
-    const signer = new ethers.Wallet(PRIVATE_KEY, provider);
-    const wethContract = new ethers.Contract(WETH_MAINNET, ABI_WETH, signer);
-
     const tx = await wethContract.deposit({ value: ethers.parseEther("10") });
     await tx.wait();
 
@@ -21,12 +21,21 @@ async function depositEth(){
     console.log("Weth Balance: " + ethers.formatEther(balance));
 }
 
+async function payFirstMonth(cerberusPay: CerberusPay) {
+    await wethContract.approve(cerberusPay.target, ethers.parseEther("1"));
+    await cerberusPay.pay(WALLET);
+    console.log(`Account 0 payed!`);
+}
+
 async function main() {
     const cerberusPay = await ethers.deployContract("CerberusPay");
     await cerberusPay.waitForDeployment();
+    await cerberusPay.setAcceptedToken(WETH_MAINNET);
     console.log(`Deployed to ${cerberusPay.target}`);
 
     await depositEth();
+
+    await payFirstMonth(cerberusPay);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
